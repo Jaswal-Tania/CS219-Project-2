ab4e9dcacdfe29dba3f352cef723a293cd0299fb
Tania Jaswal <tjaswal@nevada.unr.edu>
2022-03-01 12:11:57 -0800

    hw 2

--- main.cpp
+++ main.cpp
@@ -36,40 +36,64 @@ int main(){
             //if operation is "ADD"
             if(operation == "ADD"){
             result = operand1 + operand2;
-            
+
             cout << hex << "The sum of Ox" << operand1 << " and Ox" << operand2 << " is Ox" << result << endl;
             }
 
             else if(operation == "AND"){
-
+                    result = operand1 & operand2;
+                  
+                // printing result
+                cout << hex << "The AND of Ox" << operand1 << " and Ox " << operand2 << " is Ox" << result << endl;
             }
 
             else if(operation == "ASR"){
-
+                 result = operand1 >> 1;
+                  
+                // printing result
+                cout << hex << "The ASR of Ox" << operand1 << " is Ox" << result << endl;
             }
 
             else if(operation == "LSR"){
-
+                 result = operand1 >>> 1;
+                  
+                // printing result
+                cout << hex << "The LSR of Ox" << operand1 << " is Ox" << result << endl;
             }
 
             else if(operation == "LSL"){
-
+                 result = operand1 <<< 1;
+                  
+                // printing result
+                cout << hex << "The LSL of Ox" << operand1 << " is Ox" << result << endl;
             }
 
             else if(operation == "NOT"){
-
+                result = ~operand1 ;
+                  
+                // printing result
+                cout << hex << "The NOT of Ox" << operand1 << " is Ox" << result << endl;
             }
 
             else if(operation == "ORR"){
-
+                 result = operand1 | operand2;
+                  
+                // printing result
+                cout << hex << "The OR of Ox" << operand1 << " and Ox " << operand2 << " is Ox" << result << endl;
             }
 
             else if(operation == "SUB"){
-
+                result = operand1 - operand2;
+                  
+                // printing result
+                cout << hex << "The SUB of Ox" << operand1 << " and Ox " << operand2 << " is Ox" << result << endl;
             }
 
             else if(operation == "XOR"){
-
+                 result = operand1 ^ operand2;
+                  
+                // printing result
+                cout << hex << "The XOR of Ox" << operand1 << " and Ox " << operand2 << " is Ox" << result << endl;
             }
         }
         myfile.close(); 
